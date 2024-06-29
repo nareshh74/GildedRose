de54e7109da7c5363b4569477fadb633a8441530
Naresh Kumar <kumarnaresh@microsoft.com>
2024-06-29 23:29:49 +0530

    move method
    Removed ItemExtensions.cs and updated GildedRose.Console.csproj to exclude it from compilation. Moved the update logic into a new Update method within the Item class, handling specific rules for different item types directly.

--- src/GildedRose.Console/Item.cs
+++ src/GildedRose.Console/Item.cs
@@ -7,6 +7,78 @@
         public int SellIn { get; set; }
 
         public int Quality { get; set; }
-    }
 
+        public virtual void Update()
+        {
+            if (this.Name != "Aged Brie" && this.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (this.Quality > 0)
+                {
+                    if (this.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        this.Quality = this.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+
+                    if (this.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.SellIn < 11)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+
+                        if (this.SellIn < 6)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (this.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                this.SellIn = this.SellIn - 1;
+            }
+
+            if (this.SellIn < 0)
+            {
+                if (this.Name != "Aged Brie")
+                {
+                    if (this.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.Quality > 0)
+                        {
+                            if (this.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                this.Quality = this.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this.Quality = this.Quality - this.Quality;
+                    }
+                }
+                else
+                {
+                    if (this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+                    }
+                }
+            }
+        }
+    }
 }
