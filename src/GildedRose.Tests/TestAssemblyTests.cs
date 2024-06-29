7c8b119346c4a9f906b40e34b1fa6b071122db81
Naresh Kumar <kumarnaresh@microsoft.com>
2024-06-29 22:54:43 +0530

    add tests

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,6 +1,7 @@
 using GildedRose.Console;
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Sdk;
 
 namespace GildedRose.Tests
 {
@@ -21,6 +22,116 @@ namespace GildedRose.Tests
             };
             app.UpdateQuality();
             Assert.True(true);
+        }
+
+        [Theory]
+        [InlineData(21, 20)]
+        [InlineData(22, 10)]
+        [InlineData(23, 4)]
+        [InlineData(0, 0)]
+        public void TestItem_Backstage(int expectedQuality, int sellIn)
+        {
+            // Arrange
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = sellIn,
+                        Quality = 20
+                    }
+                }
+            };
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.Equal(expectedQuality, app.Items[0].Quality);
+            Assert.Equal(sellIn - 1, app.Items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(21, 4)]
+        [InlineData(22, 0)]
+        public void TestItem_AgedBrie(int expectedQuality, int sellIn)
+        {
+            // Arrange
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        Name = "Aged Brie",
+                        SellIn = sellIn,
+                        Quality = 20
+                    }
+                }
+            };
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.Equal(expectedQuality, app.Items[0].Quality);
+            Assert.Equal(sellIn - 1, app.Items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(20, 20)]
+        [InlineData(20, -1)]
+        public void TestItem_Sulfuras(int expectedQuality, int sellIn)
+        {
+            // Arrange
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        Name = "Sulfuras, Hand of Ragnaros",
+                        SellIn = sellIn,
+                        Quality = 20
+                    }
+                }
+            };
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.Equal(expectedQuality, app.Items[0].Quality);
+            Assert.Equal(sellIn, app.Items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(19, 20)]
+        [InlineData(18, 0)]
+        public void TestItem_NormalItem(int expectedQuality, int sellIn)
+        {
+            // Arrange
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        Name = "Normal Item",
+                        SellIn = sellIn,
+                        Quality = 20
+                    }
+                }
+            };
+
+            // Act
+            app.UpdateQuality();
+
+            // Assert
+            Assert.Equal(expectedQuality, app.Items[0].Quality);
+            Assert.Equal(sellIn - 1, app.Items[0].SellIn);
         }
     }
 }
