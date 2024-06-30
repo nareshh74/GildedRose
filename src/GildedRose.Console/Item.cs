5d37b596edb89ed7609ca6cc00c854abb286d906
Naresh Kumar <kumarnaresh@microsoft.com>
2024-06-30 23:17:53 +0530

    Refactor: Move Sulfuras class to its own file
    The Sulfuras class, which represents the "Sulfuras, Hand of Ragnaros" item, has been moved from Item.cs to a new file named Sulfuras.cs. The GildedRose.Console.csproj file has been updated accordingly. The functionality of the Sulfuras class remains unchanged.

--- src/GildedRose.Console/Item.cs
+++ src/GildedRose.Console/Item.cs
@@ -26,19 +26,4 @@
             }
         }
     }
-
-    public class Sulfuras : Item
-    {
-        public Sulfuras(int sellIn, int quality) : base()
-        {
-            this.Name = "Sulfuras, Hand of Ragnaros";
-            this.SellIn = sellIn;
-            this.Quality = quality;
-        }
-
-        public override void Update()
-        {
-            // Sulfuras never has to be sold or decreases in Quality
-        }
-    }
 }
---
+++ src/GildedRose.Console/Sulfuras.cs
@@ -0,0 +1,17 @@
+﻿namespace GildedRose.Console
+{
+    public class Sulfuras : Item
+    {
+        public Sulfuras(int sellIn, int quality) : base()
+        {
+            this.Name = "Sulfuras, Hand of Ragnaros";
+            this.SellIn = sellIn;
+            this.Quality = quality;
+        }
+
+        public override void Update()
+        {
+            // Sulfuras never has to be sold or decreases in Quality
+        }
+    }
+}
